a2c40c1dbf3aa36eb2979207323c6bae5dcb239a
erikhuallpa <erikhuallpa22@gmail.com>
2022-05-21 10:31:15 -0400

    Creando la tabla compra con relaciones

---
+++ database/migrations/2022_05_21_101655_create_buy_products_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBuyProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('buy_products', function (Blueprint $table) {
+            $table->id('id_buy_product');
+            $table->unsignedBigInteger('id_product');
+            $table->integer('quantity');
+            $table->tinyInteger('state');
+            $table->timestamps();
+            $table->foreign('id_product')->references('id_product')->on('products')->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('buy_products');
+    }
+}
